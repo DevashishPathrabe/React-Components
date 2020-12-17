0dc9aae2ce4a4856e8736a76b1d288dfb3d9ca99
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-12-17 20:41:56 +0530

    A Button start with label 5, then double value on each click

---
+++ A Button start with label 5, then double value on each click.js
@@ -0,0 +1,12 @@
+function Button() {
+  const [counter, setCounter] = useState(5);
+  const handleClick = () => setCounter(counter*2);
+	return <button onClick={handleClick}>
+    {counter}
+  </button>;
+}
+
+ReactDOM.render(
+  <Button />, 
+  document.getElementById('mountNode'),
+);
